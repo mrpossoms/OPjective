53035222531e53cd521ac2a34402c0e0847f78cf
Kirk Roerig <mr.possoms@gmail.com>
2014-12-29 08:55:54 -0500

    Added added Touchable protocol. Reworked states to accomodate gestures

---
+++ OPjective/Protocols/Touchable.h
@@ -0,0 +1,13 @@
+//
+//  Touchable.h
+//  OPjective
+//
+//  Created by Kirk Roerig on 12/27/14.
+//  Copyright (c) 2014 OPifex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Touchable <NSObject>
+
+@end
